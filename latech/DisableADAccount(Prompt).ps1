1122eb8bf2781123456c71dbb9193378106d5657
sredfe <65383222+sredfe@users.noreply.github.com>
2022-02-27 00:40:04 -0600

    Active Directory Remove & Disable Scripts
    Helper scripts to remove or disable users in active directory

---
+++ latech/DisableADAccount(Prompt).ps1
@@ -0,0 +1,10 @@
+﻿#Import AD Module
+Import-Module ActiveDirectory
+
+#Gets the previous date
+$lastdate= (Get-Date).AddDays(-180)
+
+#Get-ADUser -Properties LastLogonDate -Filter {LastLogonDate -lt $lastdate } | Remove-ADUser –WhatIF
+$name = Read-Host -Prompt "Insert Name"
+$identity = (Get-ADUser -Filter "Name -eq '$name'").DistinguishedName | Disable-ADAccount
+echo "$name disabled"  
---
+++ latech/RemoveADUser(Prompt).ps1
@@ -0,0 +1,15 @@
+﻿#Import AD Module
+Import-Module ActiveDirectory
+
+#prompt user for CSV file path
+$firstname =Read-Host -Prompt "Please enter the first name of the user: "
+$lastname =Read-Host -Prompt "Please enter the last name of the user: "
+
+#Sets the full name
+$name = "$firstname $lastname"
+
+#Grabs the Distinguished Name that Remove-ADUser needs to work 
+$identity = (Get-ADUser -Filter "Name -eq '$name'").DistinguishedName
+
+Remove-ADUser -Identity $identity -Confirm:$false
+echo "$name deleted" 
