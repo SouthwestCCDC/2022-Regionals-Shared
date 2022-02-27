b02ee6b5db8ce92ade1a49d17a845b8cd6bd07cb
sredfe <65383222+sredfe@users.noreply.github.com>
2022-02-26 21:37:29 -0600

    Active Directory New User Powershell Script
    Powershell scripts to add new users to active directory from either CSV files or command line prompt

---
+++ latech/CreateADUser(CSV).ps1
@@ -0,0 +1,44 @@
+﻿#Import AD Module
+Import-Module ActiveDirectory
+
+#Notes
+#(thing next to the one) allows you yo continue script on the next line/ NOTE NEEDED AFTER EVERY LINE. WILL NOT WORK IF A SPACE FOLLOWS IT
+#-Name = Full Name (First, Last)
+#-GivenName = First Name
+#-Surname = Last Name
+#-UserPrincipalName = User Name
+#-AccountPassword (ConvertTo-SecureString [string] -AsPlainText -Force) = Password
+#-Path = Path (In AD with view Advanced features on -> right click on the folder -> properties -> attribute editor -> distinguished name)
+#-ChangePasswordAtLogon 1 = Make new user change password at log on. (0 = False, 1 = True)
+#-Enabled 1 = Determines if the account is disabled by default
+
+#prompt user for CSV file path
+$filepath = Read-Host -Prompt "Please enter the path to you CSV file"
+
+#import the file into a variable
+$users = Import-Csv $filepath
+
+ForEach ($user in $users){
+#Use the column name after $user
+$firstname = $user."first_name"
+$lastname = $user."last_name"
+$department = $user."Department"
+$jobtitle = $user."jobtitle"
+$username = $user."username"
+$password = $user."password"
+
+echo "$username, $firstname, $lastname, $jobtitle, $department"
+
+#Create AD user
+New-ADUser `
+    -Name "$firstname $lastname" `
+    -GivenName $firstname `
+    -Surname $lastname `
+    -UserPrincipalName $username `
+    -Department $department `
+    -Title $jobtitle `
+    -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
+    -Path "OU=Testing Grounds,DC=test,DC=local" `
+    -ChangePasswordAtLogon 1 `
+    -Enabled 1 
+}
